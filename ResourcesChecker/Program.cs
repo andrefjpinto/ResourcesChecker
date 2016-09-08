a1c8df4c60d94180b4afa14979a09b1e2c9d7e90
André Pinto <andre.pinto@dominos.co.uk>
2016-09-08 17:17:18 +0100

    Add project files.

---
+++ ResourcesChecker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourcesChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
