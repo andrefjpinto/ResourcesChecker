7af08ae0fc9cb528374f92be7ec8775e6db2fc3d
paulo.iap <paulo.iap@gmail.com>
2016-09-15 21:48:37 +0100

    Improve Performance
    With Parallel.ForEach e e sem RegEx... almost final version
    
    Shame

--- ResourcesChecker/Program.cs
+++ ResourcesChecker/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using LibGit2Sharp;
@@ -14,27 +15,28 @@ namespace ResourcesChecker
     public static class Program
     {
         #region Configuration
-        private const string SourcePath = @"z:\dev\git\DPG.Ecommerce";
-        private const string ResourcesFile = @"z:\dev\git\DPG.Ecommerce\Source\DPG.Ecommerce.Resources\Resource-en-GB.json";
+        private const string SourcePath = @"D:\dev\git\DPG.Ecommerce";
+        private const string ResourcesFile = @"D:\dev\git\DPG.Ecommerce\Source\DPG.Ecommerce.Resources\Resource-en-GB.json";
         private const string ResultsFileName = "results.csv";
-        private const int NumberOfThreads = 8;
         #endregion
 
         private static List<FilePath> _repositoryFiles;
         private static List<Resource> _resources;
-        private static Dictionary<string, int> _threadsInfo;
-        private static List<Task> _threadList;
+        private static List<string> _metrics;
+
+        private static StringBuilder _masterStringBuilder;
+        private static string masterString;
 
         private static int ProcessedFiles { get; set; }
 
         public static void Main(string[] args)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            
+
             _repositoryFiles = new List<FilePath>();
             _resources = new List<Resource>();
-            _threadsInfo = new Dictionary<string, int>();
-            _threadList = new List<Task>();
+            _masterStringBuilder = new StringBuilder();
+            _metrics = new List<string>();
 
             LoadRepositoryFiles();
             Console.WriteLine($"Loaded {_repositoryFiles.Count} Repository files in {watch.ElapsedMilliseconds} ms.");
@@ -45,14 +47,17 @@ namespace ResourcesChecker
 
             Console.WriteLine("Checking files...");
 
-            Task.WaitAll(_threadList.ToArray());
-
             SaveResult();
 
             watch.Stop();
-            
+
             Console.WriteLine($"Analyzed {_repositoryFiles.Count} files and {_resources.Count} resources in {watch.ElapsedMilliseconds} ms.");
             Console.WriteLine($"Unused resources list saved in {Directory.GetCurrentDirectory()}\\{ResultsFileName}");
+
+            string metricsResult = String.Join(Environment.NewLine, _metrics.ToArray());
+            File.WriteAllText("metrics.csv", metricsResult);
+
+            Console.ReadLine();
         }
 
         /// <summary>
@@ -60,67 +65,131 @@ namespace ResourcesChecker
         /// </summary>
         private static void CheckResources()
         {
-            var auxStart = 0;
+            //Parallel.ForEach(_repositoryFiles.AsReadOnly(), ProcessFile);
 
-            _threadsInfo.Add("thread0", _repositoryFiles.Count % NumberOfThreads + _repositoryFiles.Count / NumberOfThreads);
+            masterString = _masterStringBuilder.ToString().Replace(" ", string.Empty).Replace("\"", "'");
 
-            for (var i = 1; i < NumberOfThreads; i++)
+            
+
+            Parallel.ForEach(_resources, ProcessResource);
+        }
+
+        private static void ProcessResource(Resource resource)
+        {
+            var foundCS = masterString.IndexOf($"{resource.Type}ResourceDictionary.{resource.Name}", StringComparison.OrdinalIgnoreCase) > -1;
+            
+            if (!foundCS)
             {
-                _threadsInfo.Add($"thread{i}", _repositoryFiles.Count / NumberOfThreads);
-            }
+                
+                var foundJS = masterString.IndexOf($"'{resource.Type}','{resource.Name}'", StringComparison.OrdinalIgnoreCase) > -1;
 
-            foreach (var threadInfo in _threadsInfo)
-            {
-                if (auxStart < _repositoryFiles.Count)
+                if (foundJS)
                 {
-                    var aux = auxStart;
-
-                    _threadList.Add(
-                        Task.Factory.StartNew(
-                            () => FindUnusedResources(_repositoryFiles.GetRange(aux, threadInfo.Value))));
+                    resource.Matches = 1;
                 }
-                auxStart += threadInfo.Value;
             }
+            else
+            {
+                resource.Matches = 1;
+            }
+
         }
 
         private static void SaveResult()
         {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            StringBuilder sb = new StringBuilder();
+
             StreamWriter resultsfile;
             using (resultsfile = new StreamWriter(ResultsFileName))
             {
-                foreach (var source in _resources.Where(x => x.Matches == 0))
+                var resourceList = _resources.Where(x => x.Matches == 0);
+
+                foreach (var source in resourceList)
                 {
-                    resultsfile.WriteLine(source.Type + ", " + source.Name);
+                    sb.AppendLine($"{source.Type}, {source.Name}");
                 }
 
+                resultsfile.Write(sb.ToString());
+
                 resultsfile.Close();
+
+                watch.Stop();
+
+                Console.WriteLine($"Saved {resourceList.Count()} unused resources in {watch.ElapsedMilliseconds} ms.");
             }
         }
 
-        private static void FindUnusedResources(IEnumerable<FilePath> sourceFiles)
+        private static void ProcessFile(FilePath filesSource)
         {
-            foreach (var filesSource in sourceFiles)
+            if (filesSource != null)
             {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
                 var file = filesSource.Content;
 
-                foreach (var resource in _resources.Where(x => x.Matches == 0))
-                {
-                    var matches = 0;
-                    if (!filesSource.IsJavascript)
-                    {
-                        var defaultRegex = new Regex($"{resource.Type}ResourceDictionary.{resource.Name}", RegexOptions.IgnoreCase);
-                        matches = defaultRegex.Matches(file).Count;
-                    }
-                    else if (filesSource.IsJavascript)
-                    {
-                        var jsRegex = new Regex($"([\"\']){resource.Type}([\"\'])(^|, ?)([\"\']){resource.Name}([\"\'])", RegexOptions.IgnoreCase);
-                        matches = jsRegex.Matches(file).Count;
-                    }
+                //var resourceList = _resources.Where(x => x.Matches == 0);
 
-                    resource.Matches += matches;
+                foreach (var resource in _resources)
+                {
+                    if (resource.Matches == 0)
+                    {
+                        var matches = 0;
+
+                        int indexType = file.IndexOf(resource.Type, StringComparison.OrdinalIgnoreCase);
+
+                        if (indexType > 0)
+                        {
+                            int indexName = -1;
+
+                            indexName = file.IndexOf(resource.Name, indexType, StringComparison.OrdinalIgnoreCase);
+
+                            if ((indexType != -1 && indexName != -1) && (indexName - indexType < 5))
+                            {
+                                int startIndexString = indexType;
+                                int endIndexString = indexType + resource.Name.Length + 10;
+
+                                if (endIndexString > file.Length)
+                                    endIndexString = file.Length;
+
+                                string minorString = file.Substring(startIndexString, endIndexString - startIndexString);
+
+                                if (!filesSource.IsJavascript)
+                                {
+                                    //var defaultRegex = new Regex($"{resource.Type}ResourceDictionary.{resource.Name}", RegexOptions.IgnoreCase);
+                                    //matches = defaultRegex.Matches(file).Count;
+
+                                    //matches = file.IndexOf($"{resource.Type}ResourceDictionary.{resource.Name}", StringComparison.OrdinalIgnoreCase) > -1 ? 1 : 0;
+
+                                    matches = minorString.IndexOf($"{resource.Type}ResourceDictionary.{resource.Name}", StringComparison.OrdinalIgnoreCase) > -1 ? 1 : 0;
+                                }
+                                else if (filesSource.IsJavascript)
+                                {
+                                    var jsRegex = new Regex($"([\"\']){resource.Type}([\"\'])(^|, ?)([\"\']){resource.Name}([\"\'])", RegexOptions.IgnoreCase);
+                                    //matches = jsRegex.Matches(file).Count > 0 ? 1 : 0;
+                                    matches = jsRegex.Matches(minorString).Count > 0 ? 1 : 0;
+
+
+
+
+                                    //matches = (
+                                    //    file.IndexOf($"\"{resource.Type}\", \"{resource.Name}\"", StringComparison.OrdinalIgnoreCase) > -1 ||
+                                    //    file.IndexOf($"\"{resource.Type}\",\"{resource.Name}\"", StringComparison.OrdinalIgnoreCase) > -1 ||
+                                    //    file.IndexOf($"'{resource.Type}', '{resource.Name}'", StringComparison.OrdinalIgnoreCase) > -1 ||
+                                    //    file.IndexOf($"'{resource.Type}','{resource.Name}'", StringComparison.OrdinalIgnoreCase) > -1
+                                    //    ) ? 1 : 0;
+                                }
+                            }
+                        }
+
+                        resource.Matches += matches;
+                    }
                 }
 
                 ProcessedFiles++;
+
+                _metrics.Add($"{filesSource.Path}, {watch.ElapsedMilliseconds}");
             }
         }
 
@@ -150,36 +219,61 @@ namespace ResourcesChecker
         // that are found, and process the files they contain.
         private static void ProcessRepository(string repositoryPath)
         {
+            //var filePathList = new List<FilePath>();
+
             using (var repo = new Repository(repositoryPath))
             {
-                RecursivelyGetPaths(_repositoryFiles, repo.Head.Tip.Tree);
+                //RecursivelyGetPaths(filePathList, repo.Head.Tip.Tree);
+                RecursivelyGetPaths(repo.Head.Tip.Tree);
             }
+
+            //  Parallel.ForEach(filePathList, LoadFileMetadata);
         }
 
-        private static void RecursivelyGetPaths(List<FilePath> paths, Tree tree)
+        private static void RecursivelyGetPaths(
+            //List<FilePath> paths, 
+            Tree tree)
         {
             foreach (TreeEntry te in tree)
             {
-                if (!te.Path.EndsWith(".generated.cs") && (te.Path.EndsWith(".cs") || te.Path.EndsWith(".cshtml") || te.Path.EndsWith(".js")))
-                {
-                        string filepath = $"{SourcePath}\\{te.Path}";
-                        //Console.WriteLine($"Loading file: {filepath}");
-                        
-                        var fileContent = File.ReadAllText(filepath);
+                string filePath = te.Path;
 
-                        paths.Add(new FilePath()
-                        {
-                            Path = te.Path,
-                            IsJavascript = te.Path.EndsWith(".js"),
-                            Content = fileContent
-                        });
+                if (!filePath.EndsWith(".generated.cs") && !filePath.EndsWith(".Designer.cs") && (filePath.EndsWith(".cs") || filePath.EndsWith(".cshtml") || filePath.EndsWith(".js")))
+                {
+                    string fullPath = $"{SourcePath}\\{te.Path}";
+                    
+                    string fileContent = File.ReadAllText(fullPath);
+
+                    _masterStringBuilder.Append(fileContent);
+                    
+                    //_repositoryFiles.Add(new FilePath()
+                    //{
+                    //    Path = fullPath,
+                    //    IsJavascript = filePath.EndsWith(".js"),
+                    //    Content = File.ReadAllText(fullPath)
+                    //});
                 }
 
                 if (te.TargetType == TreeEntryTargetType.Tree)
                 {
-                    RecursivelyGetPaths(paths, te.Target as Tree);
+                    //RecursivelyGetPaths(paths, te.Target as Tree);
+                    RecursivelyGetPaths(te.Target as Tree);
                 }
             }
         }
+
+        //private static void LoadFileMetadata(string filePath)
+        //{
+        //    if (!filePath.EndsWith(".generated.cs") && (filePath.EndsWith(".cs") || filePath.EndsWith(".cshtml") || filePath.EndsWith(".js")))
+        //    {
+        //        _repositoryFiles.Add(new FilePath()
+        //        {
+        //            Path = filePath,
+        //            IsJavascript = filePath.EndsWith(".js")
+        //            ,
+        //            Content = File.ReadAllText(filePath)
+        //        });
+        //    }
+        //}
     }
 }
